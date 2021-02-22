11018ddb87dad3e12bca86a96feaa28bb74d2be2
DanielJDupont <ddupont2@uwo.ca>
2021-02-21 19:55:30 -0500

    wrote scale and translate with opinion of an initial value

--- components/examples/exampleAnimation/index.tsx
+++ components/examples/exampleAnimation/index.tsx
@@ -1,11 +1,28 @@
+import Anime from 'react-animejs-wrapper';
+import SearchIcon from '@material-ui/icons/Search';
+
 import styles from './index.module.scss';
 
 export const ExampleAnimation = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.animation}>Animation</div>
-        <div className={styles.code}>Code</div>
+      <div className={styles.columnContainer}>
+        <div className={styles.title}>Sliding One Element</div>
+        <div className={styles.rowContainer}>
+          <div className={styles.animation}>
+            <Anime
+              config={{
+                scale: [0, 1.5],
+                translateX: [0, 200],
+                loop: true,
+                easing: 'spring(1, 80, 10, 0)',
+              }}
+            >
+              <SearchIcon />
+            </Anime>
+          </div>
+          <div className={styles.code}>Code</div>
+        </div>
       </div>
     </div>
   );
