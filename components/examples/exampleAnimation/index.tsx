e058efe97eb21e8157f4df8b1e8d5be3b70b3735
DanielJDupont <ddupont2@uwo.ca>
2021-02-21 19:13:06 -0500

    tweaks to styling

--- components/examples/exampleAnimation/index.tsx
+++ components/examples/exampleAnimation/index.tsx
@@ -1,5 +1,12 @@
 import styles from './index.module.scss';
 
 export const ExampleAnimation = () => {
-  return <div className={styles.wrapper}>Test</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.animation}>Animation</div>
+        <div className={styles.code}>Code</div>
+      </div>
+    </div>
+  );
 };
--- components/examples/index.tsx
+++ components/examples/index.tsx
@@ -7,8 +7,8 @@ export const Examples = () => {
       <div className={styles.container}>
         <div className={styles.title}>Sample Components</div>
         <div className={styles.description}>
-          These are designed to get you using react-animejs-wrapper as quickly
-          as possible in your application.
+          Get to using Anime.js as quickly as possible in your React
+          application.
         </div>
         <div className={styles.description}>
           For more comprehensive information on what can be passed into the
