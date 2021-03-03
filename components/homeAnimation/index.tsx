4c9f2ac788354bba284ee01a2408cb8a3820cfca
DanielJDupont <ddupont2@uwo.ca>
2021-03-03 18:03:29 -0500

    updated home page a bit

--- components/homeAnimation/index.tsx
+++ components/homeAnimation/index.tsx
@@ -4,19 +4,11 @@ import Anime from 'react-animejs-wrapper';
 export const HomeAnimation = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.bigAnimation}>Big Animation</div>
+      <div className={styles.title}>React Anime.js Wrapper</div>
       <div className={styles.subtitle}>
         A library for using Anime.js in React.
       </div>
+      <div className={styles.bigAnimation} />
     </div>
   );
 };
-
-// <Anime
-// animeConfig={{
-//   translateX: 25,
-//   loop: true,
-// }}
-// >
-// Test
-// </Anime>
